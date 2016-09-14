25553b3581283b769965752ec5045f07d92dcf71
Xtal Osc <xtalosc@gmx.com>
2016-09-14 18:00:23 -0500

    adds koala to database, displays koalas on DOM

--- public/scripts/koalaHolla.js
+++ public/scripts/koalaHolla.js
@@ -1,5 +1,5 @@
 console.log( 'js' );
-
+var koalas=[];
 $( document ).ready( function(){
   console.log( 'JQ' );
   // load existing koalas on page load
@@ -9,14 +9,18 @@ $( document ).ready( function(){
   $( '#addButton' ).on( 'click', function(){
     console.log( 'in addButton on click' );
     // get user input and put in an object
-    // NOT WORKING YET :(
+    var koalaName=$('#nameIn').val();
+    var koalaAge=$('#ageIn').val();
+    var koalaSex=$('#sexIn').val();
+    var koalaTransfer=$('#readyForTransferIn').val();
+    var koalaNotes=$('#notesIn').val();
     // using a test object
     var objectToSend = {
-      name: 'testName',
-      age: 'testName',
-      sex: 'testName',
-      readyForTransfer: 'testName',
-      notes: 'testName',
+      name: koalaName,
+      age: koalaAge,
+      sex: koalaSex,
+      readyForTransfer: koalaTransfer,
+      notes: koalaNotes,
     };
     // call saveKoala with the new obejct
     saveKoala( objectToSend );
@@ -31,9 +35,12 @@ var getKoalas = function(){
     type: 'GET',
     success: function( data ){
       console.log( 'got some koalas: ', data );
+      // display on DOM with buttons that allow edit of each
+      //append array to list
+      displayKoalas(data);
     } // end success
   }); //end ajax
-  // display on DOM with buttons that allow edit of each
+  console.log('koalas array outside ajax: ',koalas);
 } // end getKoalas
 
 var saveKoala = function( newKoala ){
@@ -45,6 +52,21 @@ var saveKoala = function( newKoala ){
     data: newKoala,
     success: function( data ){
       console.log( 'got some koalas: ', data );
+      displayKoalas(data);
     } // end success
   }); //end ajax
-}
+}//end saveKoala
+
+var displayKoalas = function(koalas){
+  var viewKoalas = $('#viewKoalas');
+  //empty viewKoalas
+  viewKoalas.empty();
+  //append array to list
+  viewKoalas.append('<h2>All Koalas</h2>');
+  //loop through data array
+  for (var i = 0; i < koalas.length; i++) {
+    viewKoalas.append('<p>' + koalas[i].name + ' ' + koalas[i].age + ' '
+    + koalas[i].sex + ' ' + koalas[i].ready_for_transfer + ' ' +
+    koalas[i].notes +  ' ' + '<button class="koalaEdit">Edit</button></p>');
+  }//end for
+};//end displayKoalas
